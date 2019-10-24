f00435a058928fd3d25d3d035d13290891c86cf5
luissancar <luissnacar@yahoo.es>
2019-10-24 17:25:08 +0200

    Actualizado 2019

--- app/src/main/java/com/example/luissancar/navigationdraweractivityrecilerview/RecyclerFragment.kt
+++ app/src/main/java/com/example/luissancar/navigationdraweractivityrecilerview/RecyclerFragment.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_recycler.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// actualizado
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
